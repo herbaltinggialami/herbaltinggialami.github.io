0e0d31f2d7c31a85a42c68db7db0ccd915f6c515
herbaltinggialami <amirabintiamrizal>
2022-12-28 20:39:13 +0700

    website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#064e3b',
+        dark: '#0f172a',
+        secondary: '#27272a'
+      }
+    },
+  },
+  plugins: [],
+}
